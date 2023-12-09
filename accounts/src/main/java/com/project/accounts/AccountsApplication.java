e0b31c8fd5fb4f31657be3a34cdd059a09ca77d1
amithkashyap <amithkashyaprv@gmail.com>
2023-12-09 17:22:03 +0530

    added account repository and annotations in the application class

--- accounts/src/main/java/com/project/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/project/accounts/AccountsApplication.java
@@ -1,9 +1,36 @@
 package com.project.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account microservices REST API documentation",
+				description = "Bank application microservice REST API documentation",
+				version = "v0.0.1",
+				contact = @Contact(
+						name = "Amith Kashyap H",
+						email = "amith@gmail.com",
+						url = "amith.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "amith.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				url = "externaldocs.com",
+				description = "Bank's Accounts microservice REST API documentation"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
