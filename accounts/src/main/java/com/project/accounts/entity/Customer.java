493b6d230263ca3df26f3166810a1209a6e7aaf4
amithkashyap <amithkashyaprv@gmail.com>
2024-05-01 03:05:27 +0530

    Added Customer entity

---
+++ accounts/src/main/java/com/project/accounts/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.project.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
