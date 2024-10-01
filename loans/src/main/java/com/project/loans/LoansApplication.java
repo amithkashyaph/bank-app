8fce5519ab388c962aea1748aa4287221a13ecc4
amithkashyap <amithkashyaprv@gmail.com>
2024-10-01 22:48:01 +0530

    enabled jpa auditing

--- loans/src/main/java/com/project/loans/LoansApplication.java
+++ loans/src/main/java/com/project/loans/LoansApplication.java
@@ -13,6 +13,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfo.class})
 public class LoansApplication {
 
 	public static void main(String[] args) {
