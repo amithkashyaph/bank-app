976ccebe9d61b33b9e21671410fbc06e56bd8005
amithkashyap <amithkashyaprv@gmail.com>
2024-10-01 22:46:24 +0530

    added loan applicationmain class

---
+++ loans/src/main/java/com/project/loans/LoansApplication.java
@@ -0,0 +1,21 @@
+package com.project.loans;
+
+import com.project.loans.dto.LoansContactInfo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
